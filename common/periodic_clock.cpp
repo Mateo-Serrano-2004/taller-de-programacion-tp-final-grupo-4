1dbc9f2378f7c7d9e5370c695974f7446891bca7
NVRar <113725464+NVRar@users.noreply.github.com>
2025-05-31 18:53:40 -0300

    acomodo de lógica

--- common/periodic_clock.cpp
+++ common/periodic_clock.cpp
@@ -23,42 +23,9 @@ uint64_t PeriodicClock::sleep_and_get_frames() {
         frames_to_process = (lost / rate) + 1;
     } else {
         frames_to_process = 1;
-        std::this_thread::sleep_for(ms(rest_time));
-        start += ms(rate);
     }
+    std::this_thread::sleep_for(ms(rest_time));
+    start += ms(rate);
 
     return frames_to_process;
 }
-
-/*PeriodicClock::PeriodicClock(uint16_t fps, std::function<void(uint64_t)> on_tick) {
-    bool running = true;
-    uint64_t frames_to_process = 1;
-    uint64_t rate = (uint64_t)(1000 / fps);
-    int64_t rest_time = 0;
-    uint64_t behind = 0;
-    uint64_t lost = 0;
-    time_point end;
-    time_point start = steady_clock::now();
-
-    while (running) {
-        try {
-            on_tick(frames_to_process);
-            end = steady_clock::now();
-            rest_time = rate - duration_cast<ms>(end - start).count();
-            if (rest_time < 0) {
-                behind = -rest_time;
-                rest_time = rate - (behind % rate);
-                lost = behind + rest_time;
-                start += ms(lost);
-                frames_to_process = (uint64_t)(lost / rate) + 1;
-            } else {
-                frames_to_process = 1;
-            }
-            std::this_thread::sleep_for(ms(rest_time));
-            start += ms(rate);
-            
-        } catch (std::exception&) {
-            running = false;
-        }
-    }
-}*/
