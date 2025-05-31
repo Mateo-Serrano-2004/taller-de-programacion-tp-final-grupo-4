8f0f2d36df81c1596ef6da03ff514b1cb8abc4ac
NVRar <113725464+NVRar@users.noreply.github.com>
2025-05-31 18:40:31 -0300

    acomo con método

--- common/periodic_clock.cpp
+++ common/periodic_clock.cpp
@@ -7,9 +7,32 @@
 
 #include "definitions.h"
 
-PeriodicClock::PeriodicClock(uint16_t fps, std::function<void()> on_tick) {
+PeriodicClock::PeriodicClock(uint16_t fps)
+    : rate(1000 / fps), start(steady_clock::now()) {}
+
+uint64_t PeriodicClock::sleep_and_get_frames() {
+    time_point end = steady_clock::now();
+    int64_t rest_time = rate - std::chrono::duration_cast<ms>(end - start).count();
+
+    uint64_t frames_to_process;
+    if (rest_time < 0) {
+        uint64_t behind = -rest_time;
+        rest_time = rate - (behind % rate);
+        uint64_t lost = behind + rest_time;
+        start += ms(lost);
+        frames_to_process = (lost / rate) + 1;
+    } else {
+        frames_to_process = 1;
+        std::this_thread::sleep_for(ms(rest_time));
+        start += ms(rate);
+    }
+
+    return frames_to_process;
+}
+
+/*PeriodicClock::PeriodicClock(uint16_t fps, std::function<void(uint64_t)> on_tick) {
     bool running = true;
-    uint64_t frame = 0;
+    uint64_t frames_to_process = 1;
     uint64_t rate = (uint64_t)(1000 / fps);
     int64_t rest_time = 0;
     uint64_t behind = 0;
@@ -19,7 +42,7 @@ PeriodicClock::PeriodicClock(uint16_t fps, std::function<void()> on_tick) {
 
     while (running) {
         try {
-            on_tick();
+            on_tick(frames_to_process);
             end = steady_clock::now();
             rest_time = rate - duration_cast<ms>(end - start).count();
             if (rest_time < 0) {
@@ -27,12 +50,15 @@ PeriodicClock::PeriodicClock(uint16_t fps, std::function<void()> on_tick) {
                 rest_time = rate - (behind % rate);
                 lost = behind + rest_time;
                 start += ms(lost);
-                frame += (uint64_t)(lost / rate);
+                frames_to_process = (uint64_t)(lost / rate) + 1;
+            } else {
+                frames_to_process = 1;
             }
             std::this_thread::sleep_for(ms(rest_time));
             start += ms(rate);
+            
         } catch (std::exception&) {
             running = false;
         }
     }
-}
+}*/
--- common/periodic_clock.h
+++ common/periodic_clock.h
@@ -1,4 +1,27 @@
-#ifndef CLIENT_GAME_PERIODIC_CLOCK_H
+
+#ifndef PERIODIC_CLOCK_H
+#define PERIODIC_CLOCK_H
+
+#include <chrono>
+#include <cstdint>
+
+using steady_clock = std::chrono::steady_clock;
+using time_point = steady_clock::time_point;
+using ms = std::chrono::milliseconds;
+
+class PeriodicClock {
+public:
+    explicit PeriodicClock(uint16_t fps);
+
+    uint64_t sleep_and_get_frames();
+
+private:
+    uint64_t rate;
+    time_point start;
+};
+
+#endif  // PERIODIC_CLOCK_H
+/*#ifndef CLIENT_GAME_PERIODIC_CLOCK_H
 #define CLIENT_GAME_PERIODIC_CLOCK_H
 
 #include <cstdint>
@@ -6,9 +29,10 @@
 
 class PeriodicClock {
 public:
-    PeriodicClock(uint16_t fps, std::function<void()> on_tick);
+    PeriodicClock(uint16_t fps, std::function<void(uint64_t)> on_tick);
 
     ~PeriodicClock() = default;
 };
 
-#endif  // CLIENT_GAME_PERIODIC_CLOCK_H
+#endif  // CLIENT_GAME_PERIODIC_CLOCK_H*/
+
