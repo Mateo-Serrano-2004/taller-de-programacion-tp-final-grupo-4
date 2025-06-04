1460e0e5ccbedd2eecab7f04fef9c6f7a10282de
NVRar <113725464+NVRar@users.noreply.github.com>
2025-06-04 13:02:40 -0300

    basico con mapa hardcodeado

---
+++ server/game/movement_system.h
@@ -0,0 +1,16 @@
+#ifndef MOVEMENT_SYSTEM_H
+#define MOVEMENT_SYSTEM_H
+
+#include <map>
+#include <cstdint>
+#include "model/movable_player.h"
+#include <iostream>
+
+class MovementSystem {
+public:
+    MovementSystem() = default;
+
+    void process_movements(std::map<uint8_t, Model::MovablePlayer>& players, uint16_t frames_to_process);
+};
+
+#endif  // MOVEMENT_SYSTEM_H
