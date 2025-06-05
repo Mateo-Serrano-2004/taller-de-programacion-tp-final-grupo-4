a50c9d8787859a56343433b145bb542d9b6539fb
Mateo-Serrano-2004 <mvserrano@fi.uba.ar>
2025-06-05 12:53:19 +0000

    Elimino los render context

--- client/game/render/render_context.cpp
+++
@@ -1,7 +0,0 @@
-#include "render_context.h"
-
-View::RenderContext::RenderContext(SDL2pp::Window& window, SDL2pp::Renderer& renderer,
-                                         Model::AssetManager& asset_manager)
-                                         : window(window),
-                                           renderer(renderer),
-                                           asset_manager(asset_manager) {}
