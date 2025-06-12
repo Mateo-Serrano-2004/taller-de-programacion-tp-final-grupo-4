c683b2bce4dad1d2b2c4e4370c0b75702c7efa8e
NVRar <113725464+NVRar@users.noreply.github.com>
2025-06-12 04:06:16 -0300

    process shooting mejorado, falta shotmanager de verdad

--- server/game/game_logic.h
+++ server/game/game_logic.h
@@ -9,11 +9,14 @@
 #include "round.h"
 #include "shop.h"
 #include "weapon_factory.h"
+#include "server/game/model/impact.h"
+#include "shot_manager.h"
+
 
 class GameLogic {
 private:
     Shop shop;
-
+    void apply_impacts(const std::vector<Impact>& impacts, Round& round, std::map<uint8_t, FullPlayer>& players) const;
 public:
     GameLogic() = default;
 
@@ -21,7 +24,7 @@ public:
     void start_using_weapon(FullPlayer& player, const Round& round) const;
     void stop_using_weapon(FullPlayer& player) const;
     void process_shooting(std::map<uint8_t, FullPlayer>& players, Round& round, uint16_t frames_to_process) const;
-
+    
     ~GameLogic() = default;
 };
 
