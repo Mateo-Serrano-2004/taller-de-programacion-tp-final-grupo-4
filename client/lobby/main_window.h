97a7d192088e00acf94dccffed8991444b1dd65e
Mateo-Serrano-2004 <mvserrano@fi.uba.ar>
2025-06-16 21:50:14 +0000

    Añado archivo faltante de último commit

--- client/lobby/main_window.h
+++ client/lobby/main_window.h
@@ -2,9 +2,6 @@
 #define MAIN_WINDOW_H
 
 #include <QMainWindow>
-#include <QMediaPlayer>
-#include <QMediaPlaylist>
-#include <QAudioOutput>
 
 #include "common/definitions.h"
 
@@ -39,8 +36,6 @@ private slots:
 
 private:
     Ui::MainWindow* ui;
-    QMediaPlayer* musicPlayer;
-    QAudioOutput* audioOutput;
     WelcomeScene* welcomeScene = nullptr;
     LobbyScene* lobbyScene = nullptr;
     GameCreationScene* gameCreationScene = nullptr;
