8412622b47b227698fd084a0e67977e42ee97e69
Mateo-Serrano-2004 <mvserrano@fi.uba.ar>
2025-06-17 21:33:22 +0000

    Meto la lógica de carga de texturas en un asset loader

--- client/game/cs2d_app.cpp
+++ client/game/cs2d_app.cpp
@@ -28,6 +28,7 @@
 #include "context/shop_context.h"
 #include "context/end_of_game_context.h"
 
+#include "asset/asset_loader.h"
 #include "asset/asset_manager.h"
 #include "asset/asset_generator.h"
 #include "asset/font_id.h"
@@ -36,109 +37,6 @@
 #include "controller/base_controller.h"
 
 #include "utils/enum_translator.h"
-
-const std::vector<std::string> player_sprites = {
-    "ct1.bmp", "ct2.bmp", "ct3.bmp",
-    "ct4.bmp", "t1.bmp",  "t2.bmp",
-    "t3.bmp",  "t4.bmp"
-};
-
-const std::vector<std::string> weapon_sprites = {
-    "ak47.bmp", "awp.bmp", "bomb_d.bmp",
-    "glock.bmp", "knife.bmp", "m3.bmp"
-};
-
-const std::vector<std::string> hud_textures = {
-    "hud_nums.bmp", "hud_symbols.bmp"
-};
-
-void App::CS2DApp::load_weapon_sprites(Shared<Model::AssetManager> asset_manager) {
-    Model::AssetAddresser asset_addresser;
-
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_AK47, asset_addresser.get_weapon_sprite_path(weapon_sprites[0])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_AWP, asset_addresser.get_weapon_sprite_path(weapon_sprites[1])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_BOMB, asset_addresser.get_weapon_sprite_path(weapon_sprites[2])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_GLOCK, asset_addresser.get_weapon_sprite_path(weapon_sprites[3])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_KNIFE, asset_addresser.get_weapon_sprite_path(weapon_sprites[4])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_M3, asset_addresser.get_weapon_sprite_path(weapon_sprites[5])
-    );
-}
-
-void App::CS2DApp::load_player_sprites(Shared<Model::AssetManager> asset_manager) {
-    Model::AssetAddresser asset_addresser;
-
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_CT1, asset_addresser.get_player_sprite_path(player_sprites[0])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_CT2, asset_addresser.get_player_sprite_path(player_sprites[1])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_CT3, asset_addresser.get_player_sprite_path(player_sprites[2])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_CT4, asset_addresser.get_player_sprite_path(player_sprites[3])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_T1, asset_addresser.get_player_sprite_path(player_sprites[4])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_T2, asset_addresser.get_player_sprite_path(player_sprites[5])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_T3, asset_addresser.get_player_sprite_path(player_sprites[6])
-    );
-    asset_manager->load_texture(
-        Model::TextureID::SPRITE_T4, asset_addresser.get_player_sprite_path(player_sprites[7])
-    );
-}
-
-void App::CS2DApp::load_hud_texture(
-    Shared<Model::AssetManager> asset_manager,
-    Shared<SDL2pp::Renderer> renderer,
-    const std::string& texture_name,
-    Model::TextureID texture_id
-) {
-    Model::AssetAddresser asset_addresser;
-
-    SDL2pp::Surface surface(asset_addresser.get_hud_resource(texture_name));
-
-    surface.SetColorKey(
-        true,
-        SDL_MapRGB(surface.Get()->format, 0, 0, 0)
-    );
-
-    auto texture = make_shared<SDL2pp::Texture>(*renderer, surface);
-
-    asset_manager->load_texture(
-        texture_id, texture
-    );
-}
-
-void App::CS2DApp::load_hud_textures(
-    Shared<Model::AssetManager> asset_manager,
-    Shared<SDL2pp::Renderer> renderer
-) {
-    load_hud_texture(asset_manager, renderer, hud_textures[0], Model::TextureID::HUD_NUMS);
-    load_hud_texture(asset_manager, renderer, hud_textures[1], Model::TextureID::HUD_SYMBOLS);
-}
-
-void App::CS2DApp::load_generated_textures(Shared<Model::AssetManager> asset_manager,
-                                    Shared<SDL2pp::Renderer> renderer) {
-    View::AssetGenerator asset_generator(renderer);
-    asset_manager->load_texture(Model::TextureID::FOV, asset_generator.generate_fov());
-}
 
 App::CS2DApp::CS2DApp(Shared<Net::ClientProtocol> protocol): App::Application() {
     Model::EnumTranslator::build();
@@ -152,15 +50,14 @@ App::CS2DApp::CS2DApp(Shared<Net::ClientProtocol> protocol): App::Application() 
     renderer->SetLogicalSize(640, 480);
 
     auto asset_manager = make_shared<Model::AssetManager>(renderer);
-    load_weapon_sprites(asset_manager);
-    load_player_sprites(asset_manager);
-    load_hud_textures(asset_manager, renderer);
-    load_generated_textures(asset_manager, renderer);
+    auto asset_loader = make_shared<Model::AssetLoader>(asset_manager, renderer);
+    asset_loader->load_all_textures();
 
     context_manager = make_shared<Context::ContextManager>();
     auto game_controller = make_shared<Controller::GameController>(
         window, renderer, asset_manager, context_manager, protocol
     );
+
     auto weak_game_controller = Weak<Controller::GameController>(game_controller);
     game_controller->set_self_pointer(weak_game_controller);
 
